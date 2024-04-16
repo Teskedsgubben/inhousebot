2c8f9e2ba9d724eca2cd34749ed6ffe6237a8546
pheolix <f3liix@hotmail.com>
2024-04-16 09:46:36 +0200

    added date to backup log

--- gitbackup.sh
+++ gitbackup.sh
@@ -1,3 +1,4 @@
+printf '%(%Y-%m-%d)T\n' -1 >> /home/felix/inhousebot/gitbackup.log
 git stage games.json users.json >> /home/felix/inhousebot/gitbackup.log
 git commit -m auto_data_backup >> /home/felix/inhousebot/gitbackup.log
 git push >> /home/felix/inhousebot/gitbackup.log
